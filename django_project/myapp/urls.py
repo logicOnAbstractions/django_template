e07da55b6621a52ce1327e8825d9e36a13f284c4
logicOnAbstractions <logiconabstractions@gmail.com>
2020-12-15 18:40:22 -0500

    We draw everything from base.html will proper css/js links defined there.
    Then we have a model defined (MyObject). We can list its collection members, create new ones, show details of a specific one.
    
    Using FormModel for quicker dev.

--- django_project/myapp/urls.py
+++ django_project/myapp/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = 'myapp'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('listview/', views.MyListView.as_view(), name='mylistview'),
+    path('listview/<int:pk>/', views.ListElementView.as_view(), name='list_element'),
+    path('listview/createobject/', views.create_product, name='create_element')
 ]
--- django_project/myapp/views.py
+++ django_project/myapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views import generic
+from .models import *
+from .forms import *
+from django import forms
 
 
 class IndexView(generic.View):
@@ -9,3 +12,26 @@ class IndexView(generic.View):
     def get(self, request):
         return render(request, self.template_name)
 
+
+class MyListView(generic.ListView):
+    template_name = 'myapp/mylistview.html'
+    model = MyObjects
+    context_object_name = 'objects'
+
+
+class ListElementView(generic.DetailView):
+    model = MyObjects
+    template_name = 'myapp/list_element.html'
+
+def create_product(request):
+    if request.method == "POST":
+        form = CreateElement(request.POST)
+        if form.is_valid():
+            # create a product
+            p = MyObjects(**form.cleaned_data)
+            p.save()
+
+            return HttpResponseRedirect('/myapp/listview')       # let's say go back to product list or something
+    else:
+        form = CreateElement()
+    return render(request, 'myapp/createlistelement.html', {'form': form})
